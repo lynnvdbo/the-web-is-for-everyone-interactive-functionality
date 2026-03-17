2ecf2c33c9baf57609ea48769c98ad68a55b21b1
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-03-17 22:30:50 +0100

    server.js #13

--- server.js
+++ server.js
@@ -5,6 +5,43 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
+const tempDummyNews = {
+  data: [
+    {
+      id: 1,
+      slug: "laatste-kans-nabloei-duizendblad",
+      title: "Laatste kans om de nabloei te zien van de duizendblad",
+      body: "De bloei van duizendblad is bijna voorbij, maar de plant is nog goed te zien. De bloemen zijn aan het opdrogen en blijven nog even zichtbaar. Dit is een laatste moment om te zien hoe duizendblad eruitziet na de bloei, voordat de bloemen niet meer zichtbaar zijn. Wat wel mooi zichtbaar blijft zijn de mooie bladeren.",
+      date: "2025-11-20",
+      image: "ccc47447-c2db-441b-a18b-183ab5a0b280"
+    },
+    {
+      id: 2,
+      slug: "zadenknoppen-teunisbloem",
+      title: "De zadenknoppen van de teunisbloem zijn nu goed te zien",
+      body: "De bloei van duizendblad is bijna voorbij, maar de plant is nog goed te zien. De bloemen zijn aan het opdrogen en blijven nog even zichtbaar. Dit is een laatste moment om te zien hoe duizendblad eruitziet na de bloei, voordat de bloemen niet meer zichtbaar zijn. Wat wel mooi zichtbaar blijft zijn de mooie bladeren.",
+      date: "2025-11-20",
+      image: "ccc47447-c2db-441b-a18b-183ab5a0b280"
+    },
+    {
+      id: 3,
+      slug: "teunisbloem-zaden-3",
+      title: "De zadenknoppen van de teunisbloem zijn nu goed te zien 3",
+      body: "De bloei van duizendblad is bijna voorbij, maar de plant is nog goed te zien. De bloemen zijn aan het opdrogen en blijven nog even zichtbaar. Dit is een laatste moment om te zien hoe duizendblad eruitziet na de bloei, voordat de bloemen niet meer zichtbaar zijn. Wat wel mooi zichtbaar blijft zijn de mooie bladeren.",
+      date: "2025-11-20",
+      image: "ccc47447-c2db-441b-a18b-183ab5a0b280"
+    },
+    {
+      id: 4,
+      slug: "teunisbloem-zaden-4",
+      title: "De zadenknoppen van de teunisbloem zijn nu goed te zien 4",
+      body: "De bloei van duizendblad is bijna voorbij, maar de plant is nog goed te zien. De bloemen zijn aan het opdrogen en blijven nog even zichtbaar. Dit is een laatste moment om te zien hoe duizendblad eruitziet na de bloei, voordat de bloemen niet meer zichtbaar zijn. Wat wel mooi zichtbaar blijft zijn de mooie bladeren.",
+      date: "2025-11-20",
+      image: "ccc47447-c2db-441b-a18b-183ab5a0b280"
+    }
+  ]
+}
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
@@ -23,6 +60,59 @@ app.engine('liquid', engine.express())
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
+// Maak een GET route voor de index (meestal doe je dit in de root, als /)
+app.get('/', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('index.liquid')
+})
+
+// !!! route naar VELDVERKENNER PAGINA !!!  
+app.get('/veldverkenner', async function (request, response) {
+  console.log(tempDummyNews)
+   response.render('veldverkenner.liquid', {nieuws: tempDummyNews.data})
+})
+
+// !!! route naar NIEUWS PAGINA !!! 
+app.get('/nieuws', async function (request, response) {
+
+    const res = await fetch('https://fdnd-agency.directus.app/items/frankendael_news');
+    const result = await res.json();
+
+    response.render('nieuws.liquid', {
+      news: result.data
+    });
+   })
+
+// !!! dit zorgt ervoor dat het artikel die je aanklikt op de nieuwspagina het goede artikel verschijnt vanuit database !!!  
+app.get('/nieuws/:slug', async function (request, response) {
+    // const artikel = tempDummyNews.data.find(item => item.slug === nieuwSlug)
+    // deze code hieronder haalt data uit database op
+    const res = await fetch('https://fdnd-agency.directus.app/items/frankendael_news/?filter[slug]=' + request.params.slug);
+    const result = await res.json();
+
+    response.render('artikel.liquid', {
+      news: result.data
+    });
+  })
+
+// !!! route naar COLLECTIE PAGINA !!!  
+app.get('/collectie', async function (request, response) {
+  console.log(tempDummyNews)
+   response.render('collectie.liquid', {nieuws: tempDummyNews.data})
+})
+
+// !!! route naar COLLECTIE NADEBLOEI PAGINA !!!  
+app.get('/nadebloei', async function (request, response) {
+  console.log(tempDummyNews)
+   response.render('nadebloei.liquid', {nieuws: tempDummyNews.data})
+})
+
+// !!! route naar COLLECTIE INDEBLOEI PAGINA !!!  
+app.get('/indebloei', async function (request, response) {
+  console.log(tempDummyNews)
+   response.render('indebloei.liquid', {nieuws: tempDummyNews.data})
+})
 
 console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
 
@@ -69,6 +159,14 @@ app.post(…, async function (request, response) {
 })
 */
 
+
+// Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
+// Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
+app.post('/', async function (request, response) {
+  // Je zou hier data kunnen opslaan, of veranderen, of wat je maar wilt
+  // Er is nog geen afhandeling van een POST, dus stuur de bezoeker terug naar /
+  response.redirect(303, '/')
+})
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
